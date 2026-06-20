0d5b9a989d86200aa8e08c4014a8c3fc3a69a251
oticstps <oticsjetson@gmail.com>
2026-06-20 13:39:45 +0700

    Delete d-kanriban/d-program directory

--- d-kanriban/d-program/routes/energyRoutesPlant2.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const EnergyControllerPlant2 = require('../controllers/EnergyControllerPlant2');
-
-router.get('/plant2/energyShiftly', EnergyControllerPlant2.getDashboardData);
-
-module.exports = router;
